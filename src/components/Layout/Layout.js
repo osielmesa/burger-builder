d30d63fb390464b842f64d956aab96d9bb353dab
Osiel Lima <osielmesa@gmail.com>
2020-07-21 20:44:26 +0200

    Adding SideDrwaer, adjusting mobile-first and media queries for toolbar and Navigation Items

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => {
-  return (
-    <Aux>
-      <Toolbar/>
-      <main className={classes.Content}>
-        {props.children}
-      </main>
-    </Aux>
-  );
+class Layout extends Component{
+  state = {
+    showSideDrawer: true
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
+
+  render(){
+    return (
+      <Aux>
+        <Toolbar/>
+        <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+
 }
 
-export default layout;
+export default Layout;
